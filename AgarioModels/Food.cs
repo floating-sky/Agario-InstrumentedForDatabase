2eacfa86b2dfeb1f16aaf622423b2e43dc416fef
Julia Thomas <juliamthomas03@gmail.com>
2023-04-08 17:13:58 -0600

    Created all the projects and classes that will be needed and implemented the logger

--- AgarioModels/Food.cs
+++ AgarioModels/Food.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AgarioModels
 {
+    [DebuggerDisplay("Food({X},{Y})")]
     internal class Food : GameObject
     {
     }
